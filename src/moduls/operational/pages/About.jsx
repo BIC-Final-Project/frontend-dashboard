4394a57f03735d38b3dcb9d48cd32da392238838
usein <useinakbar01@gmail.com>
2024-03-02 15:57:27 +0700

    fe(feat): modify pages

--- src/moduls/operational/pages/About.jsx
+++ src/moduls/operational/pages/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
     return (
       <div>
-          <h1 className="text-2xl font-bold">About</h1>
+          <h1 className="text-2xl font-bold">About Page</h1>
       </div>
     )
   }
